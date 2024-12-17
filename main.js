e22b73e721b8cdd83a9ee4f26cf55986bcbe8bf6
Igor Bezhinarov <igorbezhinarov@yandex.ru>
2024-12-17 22:33:14 +0700

    init

---
+++ main.js
@@ -0,0 +1,6 @@
+import Swiper from 'swiper/bundle';
+import { Fancybox } from "@fancyapps/ui";
+
+import './sass/_app.scss';
+import "@fancyapps/ui/dist/fancybox/fancybox.css";
+import 'swiper/css/bundle';
