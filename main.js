1a0e72eaaad432c0d50852e932ce4448e2fb5d45
Igor Bezhinarov <igorbezhinarov@yandex.ru>
2024-12-20 20:24:41 +0700

    review section has been created

--- main.js
+++ main.js
@@ -4,3 +4,13 @@ import { Fancybox } from "@fancyapps/ui";
 import './sass/_app.scss';
 import "@fancyapps/ui/dist/fancybox/fancybox.css";
 import 'swiper/css/bundle';
+
+const reviewSlider = new Swiper('.review-slider .swiper', {
+    speed: 800,
+    loop: true,
+    spaceBetween: 50,
+    navigation: {
+        prevEl: '.review-navigation__prev',
+        nextEl: '.review-navigation__next',
+    },
+});
