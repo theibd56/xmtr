34a5229d1656e6b52792680277f66f9a30a51019
Igor Bezhinarov <igorbezhinarov@yandex.ru>
2024-12-20 21:24:54 +0700

    faq section has been created

--- main.js
+++ main.js
@@ -21,3 +21,43 @@ const partnersSlider = new Swiper('.partners-slider', {
     slidesPerView: 3,
     spaceBetween: 2,
 });
+
+document.addEventListener("DOMContentLoaded", () => {
+    const faqItems = document.querySelectorAll(".faq-item");
+
+    faqItems.forEach((item) => {
+        const top = item.querySelector(".faq-item__top");
+        const text = item.querySelector(".faq-item__text");
+
+        text.style.maxHeight = "0";
+        text.style.overflow = "hidden";
+        text.style.transition = "max-height 0.3s ease";
+
+        top.addEventListener("click", () => {
+            const isOpen = text.style.maxHeight !== "0px";
+
+            top.classList.toggle("open")
+
+            faqItems.forEach((otherItem) => {
+                const otherTop = otherItem.querySelector(".faq-item__top");
+                const otherText = otherItem.querySelector(".faq-item__text");
+
+                if (otherText !== text) {
+                    otherText.style.maxHeight = "0";
+                }
+
+                if (otherTop !== top) {
+                    otherTop.classList.remove("open")
+                }
+            });
+
+            if (isOpen) {
+                text.style.maxHeight = "0";
+            } else {
+                text.style.maxHeight = "0"; // Сброс перед применением
+                void text.offsetHeight; // Принудительное обновление
+                text.style.maxHeight = `${text.scrollHeight}px`;
+            }
+        });
+    });
+});
